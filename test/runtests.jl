ef5dc3d64c234b63291f0019dec6ef928ef1b58e
genkuroki <genkuroki@gmail.com>
2020-08-15 13:11:22 +0900

    comparison of the two algorithms added

--- test/runtests.jl
+++ test/runtests.jl
@@ -1,9 +1,19 @@
 using Ising2D
+using Random: seed!
 using Test
 
 @testset "Ising2D.jl" begin
     N = 100
-    S = [ising2d!(rand_ising2d(), β_ising2d, 100) for _ in 1:N]
-    @test abs(sum(energy_density_ising2d.(S))/N + 1.37) < 0.015
-    @test abs(sum(abs.(magnetization_ising2d.(S)))/N - 0.184) < 0.07
+
+    seed!(4649373)
+    S_ifelse = [ising2d!(IfElse(), rand_ising2d(), β_ising2d, 100) for _ in 1:N]
+    @test abs(sum(energy_density_ising2d.(S_ifelse))/N + 1.37) < 0.015
+    @test abs(sum(abs.(magnetization_ising2d.(S_ifelse)))/N - 0.184) < 0.07
+
+    seed!(4649373)
+    S_multifor = [ising2d!(MultiFor(), rand_ising2d(), β_ising2d, 100) for _ in 1:N]
+    @test abs(sum(energy_density_ising2d.(S_multifor))/N + 1.37) < 0.015
+    @test abs(sum(abs.(magnetization_ising2d.(S_multifor)))/N - 0.184) < 0.07
+
+    @test S_ifelse == S_multifor
 end
